39ae5343a12f51d88b4790ccb0f682ff486f4655
Sarwar-95 <sarwarahmad164@gmail.com>
2023-02-04 22:40:41 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:newapp/Cuisines/biryani.dart';
 import 'dashboard.dart';
 
 void main() {
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: foodDelivery(),
+      //Biryani(),
     );
   }
 }
